99ca12f33dc7d00aac9139590638cd901f9ebfc4
Andrey <andreygrt2@gmail.com>
2017-07-11 11:34:02 +0300

    [backend] fixed friends list router

--- backend/routes/friend-router.js
+++ backend/routes/friend-router.js
@@ -8,28 +8,31 @@ var friends = require("../data/friends");
 var router = express.Router();
 
 router.get('', (req, res, next) => {
-
-
-    var foundFriends = _.filter(friends, (friend) =>
-        friend.user_1 !== req.user.id && friend.user_2 !== req.user.id);
+    var foundFriends = _.filter(friends, (friend) =>{
+        if(friend.user_1 === req.user.id || friend.user_2 === req.user.id) {
+            return true;
+        }
+    });
 
     foundFriends = _.map(foundFriends, (friend) => {
-        var user = _.find(users, (user) => user.id === friend.user_1 || user.id === friend.user_2);
-        return {
-            id: user.id,
-            name: user.name,
-            lastName: user.lastName,
-            age: user.age,
-            gender: user.gender
+        var friendId = friend.user_1 === req.user.id ? friend.user_2 : friend.user_1;
+        var user = _.find(users, (user) => user.id === friendId);
+        if(user) {
+            return {
+                id: user.id,
+                name: user.name,
+                lastName: user.lastName,
+                age: user.age,
+                gender: user.gender,
+                dateOfBirth: user.dateOfBirth
+            }
         }
     });
 
     foundFriends = _.filter(foundFriends, (friend) => {
-        if (req.query.name && friend.name === req.query.name) return false;
+        if (req.query.name && friend.name !== req.query.name) return false;
         return true;
-    })
-
-    // if (!user) return res.status(404).json({message: "Entered login or password is incorrect"});
+    });
 
     res.json({friends: foundFriends})
 });
